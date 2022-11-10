63db61381a147127aa204dc213e9301aa77de284
Vedant Bojjawar <vedantbit120@gst.sies.edu.in>
2022-11-11 00:03:45 +0530

    first

---
+++ AppCounter.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/Counter';
+import Hook2usestate from './components/Hook';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+      {/* <Hook2usestate/> */}
+         
+    </div>
+  );
+}
+
+export default App;
